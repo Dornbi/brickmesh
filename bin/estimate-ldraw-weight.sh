8fda30f2dc518b36c4d51a51100fdb78e507dba3
Dornbi <dornbach@gmail.com>
2016-03-31 14:50:39 +0200

    Update binary & add more scripts.

---
+++ bin/estimate-ldraw-weight.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+CLASSPATH=brickmesh.jar:protobuf-java-3.0.0-beta-2.jar
+java -cp $CLASSPATH -Xmx128m com.brickmesh.offline.LDrawWeightEstimator $@
--- bin/most-used-parts.sh
+++ bin/most-used-parts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 CLASSPATH=brickmesh.jar:protobuf-java-3.0.0-beta-2.jar
-java -cp $CLASSPATH -Xmx128m com.brickmesh.parts.LddTool $@
+java -cp $CLASSPATH -Xmx128m com.brickmesh.offline.MostUsedParts "$@"
